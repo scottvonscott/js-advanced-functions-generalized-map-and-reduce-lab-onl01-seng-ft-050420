aad3dc569e82ca218232633abefb31af9112f152
scottvonscott <skeplin17@gmail.com>
2020-08-19 14:26:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(array, func) {
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+        let ele = array[i]
+        arr.push(func(ele))
+    }
+    return arr
+}
+
+function reduce(arr, func, startingPoint) {
+    let r = (!!startingPoint) ? startingPoint : arr[0]
+    let i = (!!startingPoint) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+        r = func(arr[i], r)
+    }
+    return r
+}
